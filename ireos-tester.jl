fe80101ea8217c169804f0b44b94afd1430b59ad
exoFlare <adrian.tobisch@aon.at>
2022-03-13 19:26:21 +0100

    IREOS 0.0.1 + benchmark test

---
+++ ireos-tester.jl
@@ -0,0 +1,43 @@
+include("ireos.jl")
+using Main.Ireos
+
+using DelimitedFiles
+using Logging
+using BenchmarkTools
+using Distances
+
+function run()
+    #ENV["JULIA_DEBUG"] = Main
+    ENV["JULIA_INFO"] = Main
+
+    benchmark_result_file_name = "benchmarks/Benchmark_IREOS_" * Ireos.VERSION * ".json"
+    
+    suite = BenchmarkGroup()
+    global data_files = ["data/WBC_withoutdupl_norm", "data/mytest.txt", "data/dens-diff_12", "data/high-noise_8"]
+    global solution_files = ["data/WBC_withoutdupl_norm_solutions.csv", "data/mytest_solutions.txt", "data/dens-diff_12_solutions.csv", "data/high-noise_8_solutions.csv"]
+    clfs = ["svc", "logreg", "klr"]
+
+    global gamma_min = 0.0
+    global tol = 0.005
+
+    @assert length(data_files) == length(solution_files)
+    for i in 1:length(data_files)
+        global data = readdlm(data_files[i])
+        global solutions = readdlm(solution_files[i],',', Float64, '\n')
+        #rowwise distance
+        global gamma_max = maximum(pairwise(Euclidean(), data, dims=1))
+        suite[data_files[i]] = BenchmarkGroup()
+        for j in 1:length(clfs)
+            global clf = clfs[j]
+            suite[data_files[i]][clf] = @benchmark begin
+            res = Ireos.ireos(data, clf, gamma_min, gamma_max, tol)
+            res2 = Ireos.evaluate_solutions(res, solutions, gamma_min, gamma_max)
+            end
+        end
+    end
+    BenchmarkTools.save(benchmark_result_file_name, suite)
+end
+
+# change default for `seconds` to 2.5
+BenchmarkTools.DEFAULT_PARAMETERS.seconds = 2.50
+run()
--- ireos.jl
+++ ireos.jl
@@ -1,12 +1,15 @@
+module Ireos
+
 using ScikitLearn
+using Distances
 
 @sk_import linear_model: LogisticRegression
 @sk_import svm: SVC
 
-
+const VERSION = "0.0.1"
 const INLIER_CLASS = -1
 const OUTLIER_CLASS = 1
-const MAX_RECURSION_DEPTH = 10
+const MAX_RECURSION_DEPTH = 3
 
 #=struct IREOSSample
     index::UInt8
@@ -26,46 +29,46 @@ function ireos(X::AbstractMatrix{<:Number}, clf::String, gamma_min::Float64, gam
     aucs = Vector{Float64}()
     num_samples = size(X)[1]
     @assert num_samples == size(X)[1]
-    @debug("Started IREOS with dataset of size:", size(X))
+    @info "Started IREOS with dataset of size:", size(X), " gamma_min: $gamma_min, gamma_max: $gamma_max, tol: $tol, classifier: $clf, max_recursion_depth: $MAX_RECURSION_DEPTH"
     clf_func = get_classifier_function(clf)
     y = fill(INLIER_CLASS, num_samples)
     for i in 1:num_samples
         y[i] = OUTLIER_CLASS
         seperabilities = Dict{Float64, Float64}()
-        push!(aucs, adaptive_quads(X, y, gamma_min, gamma_max, tol, clf_func, seperabilities))
+        outlier_index = findfirst(isequal(OUTLIER_CLASS), y)
+        @debug "Started IREOS calculation of sample number: $outlier_index"
+        push!(aucs, adaptive_quads(X, y, outlier_index, gamma_min, gamma_max, tol, clf_func, seperabilities))
+        @debug "IREOS calculation of sample number: $outlier_index successful"
         y[i] = INLIER_CLASS
     end
     return aucs
 end
 
-function adaptive_quads(X, y, a::Float64, b::Float64, tol::Float64, clf_func, seperabilities)
+function adaptive_quads(X, y, outlier_index, a::Float64, b::Float64, tol::Float64, clf_func, seperabilities, current_recursion_depth = 0)
     m = (a + b) / 2
-    err_all = simpson_rule(X, y, a, b, clf_func, seperabilities)
-    err_new = simpson_rule(X, y, a, m, clf_func, seperabilities) + simpson_rule(X, y, m, b, clf_func, seperabilities)
+    err_all = simpson_rule(X, y, outlier_index, a, b, clf_func, seperabilities)
+    err_new = simpson_rule(X, y, outlier_index, a, m, clf_func, seperabilities) + simpson_rule(X, y, outlier_index, m, b, clf_func, seperabilities)
     calculated_error = abs(err_all - err_new) / 15
-    if tol < calculated_error && 0 < MAX_RECURSION_DEPTH
-        @debug "Iteration depth: {}. Criterion not reached: $calculated_error > $tol"
-        #self.current_recursion_depth += 1
-        return adaptive_quads(X, y, a, m, tol / 2, clf_func, seperabilities) + adaptive_quads(X, y, m, b, tol/2, clf_func, seperabilities)
+    if tol < calculated_error && current_recursion_depth < MAX_RECURSION_DEPTH
+        @debug "Iteration depth: $current_recursion_depth. Criterion not reached: $calculated_error > $tol"
+        return adaptive_quads(X, y, outlier_index, a, m, tol / 2, clf_func, seperabilities, current_recursion_depth+1) + adaptive_quads(X, y, outlier_index, m, b, tol/2, clf_func, seperabilities, current_recursion_depth+1)
     else
-        @debug "Termination criterion of $calculated_error < $tol reached."
+        @debug "Termination criterion of $calculated_error < $tol OR Recursion depth $current_recursion_depth / $MAX_RECURSION_DEPTH reached."
         return err_new
     end
 end
 
-function simpson_rule(X, y, a, b, clf_func, seperabilities)
+function simpson_rule(X, y, outlier_index, a, b, clf_func, seperabilities)
     h = (b - a) / 2
     for i in [a, a+h, b]
         if i in keys(seperabilities)
             continue
         end
-        clf = clf_func(X, y, i)
+        clf, current_sample = clf_func(X, y, outlier_index, i)
         p_index = findfirst(isequal(OUTLIER_CLASS), clf.classes_)
-        current_index = findfirst(isequal(OUTLIER_CLASS), y)
-        current_sample = reshape(X[current_index, :] , (size(X)[2],1))
         p_outlier = predict_proba(clf, current_sample')[p_index]
-          
-        @debug "$current_index: gamma $i : p-value: $p_outlier"
+        
+        @debug "$outlier_index: gamma $i : p-value: $p_outlier"
         seperabilities[i] = p_outlier
     end
     return (h / 3) * (seperabilities[a] + 4 * seperabilities[a + h] + seperabilities[b])
@@ -83,41 +86,49 @@ function get_classifier_function(clf::String)
     end
 end
 
-function get_logreg_clf(X, y, gamma)
+function get_logreg_clf(X, y, outlier_index, gamma)
     clf = LogisticRegression(random_state=123, tol=0.0095, max_iter=1000000)
-    R = rbf_kernel(X, gamma=gamma)
+    R = rbf_kernel(X, gamma)
      
     #self.ireos_data.set_current_sample(R[self.ireos_data.get_index()].reshape(1, -1))
-    clf.fit(R, y)
-    return clf
+    fit!(clf, R, y)
+    current_sample = reshape(R[outlier_index, :] , (size(R)[2],1))
+    return clf, current_sample
 end
 
-function get_svm_clf(X, y, gamma)
+function get_svm_clf(X, y, outlier_index, gamma)
+    #SVC cannot dea lwith gamma == 0
+    if gamma == 0.0
+        gamma = 0.0001
+    end
     clf = SVC(gamma=gamma, probability=true, C=100, random_state=123, tol=0.0095, max_iter=1000000)
     fit!(clf, X, y)
-    return clf
+    current_sample = reshape(X[outlier_index, :] , (size(X)[2],1))
+    return clf, current_sample
 end
 
-function get_klr_clf(X, y, gamma)
+function get_klr_clf(X, y, outlier_index, gamma)
     # param set closest to the paper (liblinear returns non-zero values for gamma = 0)
     clf = LogisticRegression(class_weight="balanced", tol=0.0095, solver = "saga", C=100, max_iter=1000000, random_state=123)
-    R = rbf_kernel(X, gamma=gamma)
+    R = rbf_kernel(X, gamma)
     #self.ireos_data.set_current_sample(R[self.ireos_data.get_index()].reshape(1, -1))
-    clf.fit(R, y)
-    return clf
+    fit!(clf, R, y)
+    current_sample = reshape(R[outlier_index, :] , (size(R)[2],1))
+    return clf, current_sample
 end
 
+function evaluate_solutions(ireos, solutions, gamma_min, gamma_max)
+    @info "Started IREOS Evaluation:"
+    results = Dict{Int, Float64}()
+    for i in 1:size(solutions)[1]
+        results[i] = evaluate_solution(ireos, solutions[i,:], gamma_min, gamma_max)
+    end
+    return results
+end
 
+evaluate_solution(ireos, solution, gamma_min, gamma_max) = sum(ireos .* solution) / sum(solution) / (gamma_max - gamma_min)
 
+# radial basis function: K(x, y) = exp(-gamma ||x-y||^2)
+rbf_kernel(X, gamma) = exp.(-gamma * pairwise(SqEuclidean(), X, dims=1))
 
-
-##########################
-using DelimitedFiles
-using Logging
-ENV["JULIA_DEBUG"] = Main
-data = readdlm("WBC_withoutdupl_norm")
-
-const gamma_min = 0.0001
-const gamma_max = 2.560381915956203
-const tol = 0.005
-res = @time ireos(data, "svc", gamma_min, gamma_max, tol)
+end
